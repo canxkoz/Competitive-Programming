e78faf30873833d6c5b23da9bad65e05a54b9fa1
Can Koz <canxkoz@gmail.com>
2019-06-11 20:18:26 +0300

    last version

--- Code Forces/231-A/231A Team.cpp
+++ Code Forces/231-A/231A Team.cpp
@@ -4,30 +4,24 @@ using namespace std;
 
 int main()
 {
-        #ifndef ONLINE_JUDGE
-        // for getting input from input.txt
-        freopen("input.txt", "r", stdin);
-        // for writing output to output.txt
-        freopen("output.txt", "w", stdout);
-    #endif
 
-int T;
-cin >> T;
-int count=0;
-int a,b,c;
+    int T;
+    cin >> T;
+    int count=0;
+    int a,b,c;
 
-for(int i=0;i<T;i++)
-{	
-	
-	cin >> a >> b >>c;
-	
-	if(a+b+c >1)
-	{
-		count=count+1;
-	}
-}		
+    for(int i=0;i<T;i++)
+    {
+        
+        cin >> a >> b >>c;
+        
+        if(a+b+c >1)
+        {
+            count=count+1;
+        }
+    }
 
-cout << count;
+    cout << count;
 
-return 0;
+    return 0;
 }
