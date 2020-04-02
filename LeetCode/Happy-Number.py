bdad837d6038bf57de5ffc9a9a68347def1a4a4d
Can Koz <canxkoz@gmail.com>
2020-04-02 05:02:03 -0800

    Update Happy-Number.py

--- LeetCode/Happy-Number.py
+++ LeetCode/Happy-Number.py
@@ -34,3 +34,47 @@ class Solution(object):
             keep.add(n)
             n = self.sums(n)
         return False
+
+
+#More optimal solution
+#Solution #1
+# class Solution(object):
+    # def isHappy(self, n):
+        # """
+        # :type n: int
+        # :rtype: bool
+        # """
+        # dic = {}
+        # while n!=1:
+            # m = n
+            # temp = 0
+            # while m:
+                # temp += (m%10)**2
+                # m = m/10
+            # if temp in dic: return False
+            # else: dic[temp] = 1
+            # n = temp
+        # return True
+		
+#Solution #2
+# class Solution(object):
+
+    # def isHappy(self, n):
+        # """
+        # :type n: int
+        # :rtype: bool
+        # """
+        # def helper(n):
+            # total = 0
+            # while n > 0:
+                # reminder = n % 10
+                # total += reminder ** 2
+                # n = n // 10
+            # return int(total)
+        # seen = set()
+        # while n != 1:
+            # if n in seen:
+                # return False
+            # seen.add(n)
+            # n = helper(n)
+        # return True
